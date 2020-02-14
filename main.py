8d81b10052dfe2f441dc2ad39908df6bdf98675e
Ilya Aleshin <aleshin1196@gmail.com>
2020-02-14 10:43:40 +0300

    first_commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import itertools
+
+numbers = '9876543210'
+signs = {'0':'', '1':'+', '2':'-'}
+result_sum = 200
+
+signs_list = list(itertools.product('012', repeat=len(numbers)-1))
+
+for i in signs_list:
+    result = ''
+    for j in range(len(numbers)-1):
+        result += numbers[j]
+        result += signs[i[j]]
+    result += numbers[j+1]
+
+    sum = eval(result)
+
+    if sum == result_sum:
+        print(result + " = 200")
