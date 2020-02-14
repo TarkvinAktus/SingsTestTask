23060db3cee40a0add934df07e821686d3a82504
Ilya Aleshin <aleshin1196@gmail.com>
2020-02-14 11:00:28 +0300

    first_sign_fix

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ numbers = '9876543210'
 signs = {'0':'', '1':'+', '2':'-'}
 result_sum = 200
 
-signs_list = list(itertools.product('012', repeat=len(numbers)-1))
+signs_list = list(itertools.product('012', repeat=len(numbers)))
 
 for i in signs_list:
     result = ''
     for j in range(len(numbers)-1):
-        result += numbers[j]
         result += signs[i[j]]
+        result += numbers[j]
     result += numbers[j+1]
 
     sum = eval(result)
